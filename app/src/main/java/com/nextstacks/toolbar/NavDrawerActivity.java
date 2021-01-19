e93a94c5cb2d7bb3bbd63440fe5b3b73dc78a1b7
Akash Gautam <gautamakash91@gmail.com>
2021-01-19 16:56:24 +0530

    19Jan2021 Session code

--- app/src/main/java/com/nextstacks/toolbar/NavDrawerActivity.java
+++ app/src/main/java/com/nextstacks/toolbar/NavDrawerActivity.java
@@ -1,11 +1,19 @@
 package com.nextstacks.toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class NavDrawerActivity extends AppCompatActivity {
 
@@ -17,9 +25,47 @@ public class NavDrawerActivity extends AppCompatActivity {
         Toolbar mToolbar = findViewById(R.id.tl_nav_drawer);
         setSupportActionBar(mToolbar);
 
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
 
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, mToolbar,R.string.open, R.string.close);
         drawerToggle.syncState();
+
+        NavigationView navView = findViewById(R.id.nav_view);
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+                FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+                TestFragment testFragment = new TestFragment();
+                if(item.getItemId() == R.id.action_search){
+//                    Toast.makeText(NavDrawerActivity.this, "Search clicked", Toast.LENGTH_LONG).show();
+
+//                    startActivity(new Intent(NavDrawerActivity.this, MainActivity.class));
+
+//                    Intent sampleIntent = new Intent(NavDrawerActivity.this, MainActivity.class);
+//                    startActivity(sampleIntent);
+
+                    Bundle arguments = new Bundle();
+                    arguments.putString("TITLE", "Search Fragment");
+
+                    testFragment.setArguments(arguments);
+
+                    fm.replace(R.id.fragment_container, testFragment);
+                    fm.addToBackStack(null);
+                    fm.commit();
+                }else if(item.getItemId() == R.id.action_move){
+                    Bundle arguments = new Bundle();
+                    arguments.putString("TITLE", "Move Fragment");
+                    testFragment.setArguments(arguments);
+
+                    fm.replace(R.id.fragment_container, testFragment);
+                    fm.addToBackStack(null);
+                    fm.commit();
+
+                }
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nextstacks/toolbar/TestFragment.java
@@ -0,0 +1,38 @@
+package com.nextstacks.toolbar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
+
+public class TestFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_test, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView tvTest = view.findViewById(R.id.tv_test);
+
+        Bundle data = getArguments();
+
+        String title = data.getString("TITLE");
+
+        tvTest.setText(title);
+
+    }
+}
